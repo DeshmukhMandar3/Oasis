3a6ebf05adf2c4b2169b8c03f2264ef459cf472c
guptaneha0111 <nehagupta01113@gmail.com>
2023-01-19 23:42:15 +0530

    addedfiles

--- cw_4/src/components/Home/Home.jsx
+++ cw_4/src/components/Home/Home.jsx
@@ -1,11 +1,56 @@
 import React from "react";
 import Navbar from "../Navbar/Navbar";
+import "./Home.css"
 
 const Home=()=>{
     return(
-        <div>
+        <>
 <Navbar/>
+
+<div class="just_after_nav">
+      <div class="sliding_after_nav">
+        <div class="sliding_images"></div>
+        <div class="sliding_images_name">
+          <div id="sliding_name_first">
+            <p>Refurbished Smartphones</p>
+            
+          </div>
+          <div>
+            <p>Mens Clothing</p>
+            
+          </div>
+          <div>
+            <p>Prebuzz</p>
+            
+          </div>
+          <div>
+            <p>JMAX Smartphones</p>
+           
+          </div>
+          <div>
+            <p>Intel</p>
+            
+          </div>
         </div>
+      </div>
+      <div class="recommodation_after_nav">
+        <img
+          src="https://cdn.shopclues.com/images/banners/2022/dec/06/Platinum1_SYM_06Dec22.jpg"
+          alt=""
+        />
+        <div class="single_line"></div>
+        <img
+          src="https://cdn.shopclues.com/images/banners/2022/dec/06/Platinum2_SYM_06Dec22.jpg"
+          alt=""
+        />
+        <div class="single_line"></div>
+        <img
+          src="https://cdn.shopclues.com/images/banners/2022/dec/02/Platinum3_Esha_2ndDec22.jpg"
+          alt=""
+        />
+      </div>
+    </div>
+</>
     )
 }
 export default Home
--- cw_4/src/components/Navbar/Navbar.jsx
+++ cw_4/src/components/Navbar/Navbar.jsx
@@ -1,53 +1,225 @@
 
 import React from "react";
 import "./Navbar.css"
-import {AiOutlineAndroid} from "react-icons/ai"
-import {AiOutlineApple} from "react-icons/ai"
-import {RiWindowsLine} from "react-icons/ri"
-import {BsSearch} from "react-icons/bs"
 import logoimg from "../sources/prologo.jpg"
-// import location from "../sources/location.png"
-import notification from "../sources/notification.png"
-import wishlist from "../sources/wishlist.png"
-import addtocart from "../sources/addtocart.png"
+
 const Navbar=()=>{
+   
     return (
-        <div>
-<div className="navbar_app_logo">
+        <>
+        {/* navbar 1st part */}
+      <div className="navbar_1st_part_div">
+    <div className="navbar_app_part">
+        <ul>
+            <li>Sell With Us</li>
+            <li>Call Us</li>
+            <li>Download App</li>
+            <li></li>
+        </ul>
+    </div>  
 
-   <div className="navbar_app">
-    <span>Sell With Us | </span>
-    <span>Call Us | </span>
-    <span>Download App:</span>
-<span className="nav_app_logo"><AiOutlineAndroid/></span>
-<span className="nav_app_logo"><AiOutlineApple/></span>
-<span className="nav_app_logo"><RiWindowsLine/></span>
-   </div>
+     {/* website logo */}
 
-   <div className="nav_main_logo">
-<div style={{marginLeft:"15px"}}>
-<a  href="#">
- <img src={logoimg} width="180px" height="" alt="" srcset=""/>
- </a> 
+    <div className="navbar_website_logo">
+        <a href="#">
+            <img src={logoimg} alt="logo"/>
+        </a>
+    </div>
+
+    {/* seacrh box */}
+
+    <div className="navbar_search_box">
+        <span>
+            <input type="text" placeholder="What is on your mind today?" />
+            <button className="navbar_srch_btn">Search</button>
+        </span>
+    </div> 
+    {/* address wishlist notification cart signin part */}
+    <div className="navbar_address_section">
+        <ul>
+            <li>
+            <i class="fa-solid fa-location-dot"></i>
+            </li>
+            <li>
+            <i class="far fa-bell"></i> 
+            </li>
+            <li>
+            <i class="far fa-heart"></i>
+            </li>
+            <li id="showcart">
+                <a href="#"><i class="fa-solid fa-cart-shopping"></i></a>
+                </li>
+            
+                <li id="navbar_signin_section_first">
+                <a href="#">Sign In</a>
+                <div class="navbar_hover_content_first">
+                    <div id="navbar_userHeading_part">
+                    <i class="fa-regular fa-user"></i><span>Login/Register</span>
+                        <p>Join VIP club</p>
+                    </div>
+                    <ul>
+                        <li>
+                        <i class="fa-solid fa-bag-shopping"></i>
+                            My Orders
+                        </li>
+                        <li>
+                            <i class="fa-solid fa-person-walking-arrow-loop-left"></i>
+                            My Returns
+                        </li>
+                        <li>
+                            <i class="far fa-heart"></i>
+                            Wishlist
+                        </li>
+                        <li>
+                            <i class="fas fa-user-circle"></i>
+                            My Profile
+                        </li>
+                        <li>
+                           <i class="fa-solid fa-comment-dots"></i>
+                            My Chat
+                        </li>
+                        <li>
+                        <i class="fa-solid fa-indian-rupee-sign"></i>
+                            My CluesBucks
+                        </li>
+                        <li>
+                            <i class="fa-regular fa-message"></i>
+                            My Feedback
+                        </li>
+                        <li>
+                        <i class="fa-solid fa-phone"></i>
+                            Help & Support
+                        </li>
+                        <li>
+                        <i class="fa-solid fa-store"></i>
+                            My Favorite Stores
+                        </li>
+                        {/* <li>
+                            <i class="fa-solid fa-right-from-bracket"></i>
+                            Sign Out
+                        </li> */}
+                    </ul>
+                </div>
+            </li>
+        </ul>
+    </div>   
 </div>
-<div className="nav_search_bar">
-            <form action="">
-                <button type="submit" className="nav_search_button"><BsSearch/></button>
-                <input type="text" name="" className="nav_search_bar_input" placeholder="What is on your mind today?"/>
-            <button className="nav_search_btn">Seacrh</button>
-            </form>
-        </div>
-        <div className="nav_noti_icon">
-            {/* <div><img src={location} alt="" width="35px"/></div> */}
-            <div><img src={notification} alt="" width="30px"/></div>
-            <div><img src={wishlist}  width="30px" alt=""/></div>
-            <div><img src={addtocart} width="30px" alt="" /></div>
-            <div>Sign In</div>
-        </div>
-   </div>
-            </div>
-             </div>
+{/* navbar 1st part completed */}
+
+
+{/* 2nd part navbar dropdown section start */}
+<div className="navbar_second_dropdown_main">
+    <div id="navbar_dropdown_child">
+        <ul>
+
+{/* made in india logo */}
+        <li id="navbar_madein_insia_logo">
+                <a href="">
+                    <img src="https://images.shopclues.com/images/ui/madeinindia.png" alt="madeInIndia"/>
+                </a>
+            </li>
+
+{/* Mobiles & More dropdown options */}
+<li id="navbar_dropdown1_part">
+<a href="/ProductListPage.html">Mobiles & More</a>
+<div className="navbar_menu_wrap_part">
+    <div className="navbar_ul_menu_block_part">
+</div>
+</div>
+</li>
+
+<li id="navbar_dropdown1_part">
+<a href="/ProductListPage.html">Men</a>
+<div className="navbar_menu_wrap_part">
+    <div className="navbar_ul_menu_block_part">
+</div>
+</div>
+</li>
+
+<li id="navbar_dropdown1_part">
+<a href="/ProductListPage.html">women</a>
+<div className="navbar_menu_wrap_part">
+    <div className="navbar_ul_menu_block_part">
+</div>
+</div>
+</li>
+
+<li id="navbar_dropdown1_part">
+<a href="/ProductListPage.html">home & kitchen</a>
+<div className="navbar_menu_wrap_part">
+    <div className="navbar_ul_menu_block_part">
+</div>
+</div>
+</li>
+
+<li id="navbar_dropdown1_part">
+<a href="/ProductListPage.html">appliances</a>
+<div className="navbar_menu_wrap_part">
+    <div className="navbar_ul_menu_block_part">
+</div>
+</div>
+</li>
+
+<li id="navbar_dropdown1_part">
+<a href="/ProductListPage.html">sports & more</a>
+<div className="navbar_menu_wrap_part">
+    <div className="navbar_ul_menu_block_part">
+</div>
+</div>
+</li>
+
+<li id="navbar_dropdown1_part">
+<a href="/ProductListPage.html">essentials</a>
+<div className="navbar_menu_wrap_part">
+    <div className="navbar_ul_menu_block_part">
+</div>
+</div>
+</li>
+
+<li id="navbar_dropdown1_part">
+<a href="/ProductListPage.html">offers</a>
+<div className="navbar_menu_wrap_part">
+    <div className="navbar_ul_menu_block_part">
+</div>
+</div>
+</li>
+
+<li id="navbar_dropdown1_part">
+<a href="/ProductListPage.html">global shopping</a>
+<div className="navbar_menu_wrap_part">
+    <div className="navbar_ul_menu_block_part">
+</div>
+</div>
+</li>
+ </ul>
+    </div>
+</div>
+{/* End of Mobiles & More dropdown options */}
+
+
+</>
     )
 }
 
 export default Navbar
+
+
+{/* <div id="navbar_column_1_part" className="navbar_s_col_part">
+<ul>
+<li><strong><a href="/ProductListPage.html">Smartphones & Tablets</a></strong></li>
+<li><a href="#">I KALL</a></li> 
+<li><a href="#">Mi</a></li> 
+<li><a href="#">Mi</a></li> 
+<li><a href="#">Mi</a></li> 
+<li><a href="#">Mi</a></li> 
+<li><a href="#">Mi</a></li> 
+<li><a href="#">Mi</a></li> 
+<li><a href="#">Mi</a></li> 
+<li><a href="#">Mi</a></li> 
+<li><a href="#">Mi</a></li> 
+<br />
+<li><strong>Refurbished Smartphones</strong></li>
+<br /><br />
+<li><strong>Refurbished Feature Phones</strong></li>     
+</ul>
+</div>   */}
